17cd533cd41784d1e318d9981a23b3f3f41be424
Colin Perepelken <colin@perepelken.ca>
2021-01-18 20:00:41 -0800

    Remove github link from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,13 +2,9 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <span class="icon">
-          <a :href="githubUrl"><i class="fab fa-github"></i></a>
-        </span>
-        <strong>Bike Gearing Calculator</strong> by <a href="https://colin.perepelken.ca">Colin Perepelken</a> &#169;
-        {{ currentYear }}
+        Copyright &#169; {{ currentYear }} <a href="https://colin.perepelken.ca">Colin Perepelken</a>
       </p>
-      <p><i>Made in Canada</i> <span class="icon has-text-danger"><i class="fab fa-canadian-maple-leaf"></i></span></p>
+      <small><i>Made in Canada</i> <span class="icon has-text-danger"><i class="fab fa-canadian-maple-leaf"></i></span></small>
     </div>
   </footer>
 </template>
@@ -21,11 +17,6 @@ export default {
       return new Date().getFullYear();
     }
   },
-  data() {
-    return {
-      githubUrl: "https://github.com/colinperepelken/bike-calculator"
-    }
-  }
 }
 </script>
 
