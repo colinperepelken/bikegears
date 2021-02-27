b06c9257e3e3aca2bb5218e87539448a617d5f7f
Colin Perepelken <colin@perepelken.ca>
2021-02-26 16:52:13 -0800

    BG-9 Colour table results based on value, remove y scrollbar on tables, let them be longgg

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,11 @@
-export const hex2rgba = (hex, alpha = 1) => {
+export const hex2rgba = (hex, alpha = 1, separate = false) => {
     const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+
+    if (separate) {
+        return {
+            r: r, g: g, b: b, a: alpha
+        };
+    }
+
     return `rgba(${r},${g},${b},${alpha})`;
 };
